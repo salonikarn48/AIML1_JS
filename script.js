d646ac1a5f2b8405a24c8f21f1fe2c306a27ebab
Saloni Karn <salonikrn721@gmail.com>
2026-02-24 20:00:39 +0530

    first-file_js

---
+++ script.js
@@ -0,0 +1,25 @@
+var num=10;
+console.log(num);
+console.log(typeof(num));
+num=false;
+console.log(num);
+console.log(typeof(num));
+const sym1=Symbol(4);
+console.log(sym1);
+const sym2=Symbol(4);
+console.log(sym2);
+if(sym1==sym2){
+    console.log("true");
+
+}
+else{
+    console.log("false");
+}
+var a=100;
+var b=13;
+var c=10;
+var linebreak="<br";
+result=a+b+c;
+document.write("a+b+c");
+document.write(result);
+document.write(linebreak);
