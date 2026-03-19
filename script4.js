572d38819dd23588dc46df41584571b0f493e62b
Saloni Karn <salonikrn721@gmail.com>
2026-03-20 00:30:14 +0530

    function,array,object

---
+++ script4.js
@@ -0,0 +1,15 @@
+//function
+var sayHello=function(){
+    alert('hello world');
+}
+
+//array
+var numberArray=[1,2,3];
+var animals=newArray("cat","dog","mouse","lion");
+
+//object
+var person={
+    name:'Barack Hussein Obama ||',
+    age:'51',
+    title:'44th president of united states'
+}
